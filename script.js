3df05f358a7194272c1083a2cf133ca61fe926e1
sushmit27 <sushmitkaur.w@gmail.com>
2025-01-21 15:08:36 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+// Disable automatic carousel sliding
+document.addEventListener('DOMContentLoaded', function () {
+    $('#carouselExampleIndicators').carousel({
+        interval: false // Stops auto-sliding
+    });
+});
+document.querySelectorAll('*').forEach(el => el.style.outline = '1px solid red');
