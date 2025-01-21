90ee5c333319d8a96a9c2648983167a2fea1ec14
sushmit27 <sushmitkaur.w@gmail.com>
2025-01-21 15:12:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Disable automatic carousel sliding
-document.addEventListener('DOMContentLoaded', function () {
-    $('#carouselExampleIndicators').carousel({
-        interval: false // Stops auto-sliding
-    });
-});
-document.querySelectorAll('*').forEach(el => el.style.outline = '1px solid red');
+// // Disable automatic carousel sliding
+// document.addEventListener('DOMContentLoaded', function () {
+//     $('#carouselExampleIndicators').carousel({
+//         interval: false // Stops auto-sliding
+//     });
+// });
+// document.querySelectorAll('*').forEach(el => el.style.outline = '1px solid red');
